fd14ab6f1e12e4260ce73419a69a573e053cafdf
barline20 <viorhe.merchstore@gmail.com>
2026-02-09 20:48:00 +0700

    Create questions.js

---
+++ data/questions.js
@@ -0,0 +1,38 @@
+const questions = [
+  // A. Lelah Fisik
+  { id: 1, text: "Dalam pilihan antara tidur atau mengerjakan satu tugas lagi, saya lebih memilih tidur.", dimension: "Fisik" },
+  { id: 2, text: "Badan seringkali capek, tapi sulit bisa langsung tidur nyenyak.", dimension: "Fisik" },
+  { id: 3, text: "Jam tidur saya sering kurang dari yang ideal.", dimension: "Fisik" },
+
+  // B. Lelah Pikiran
+  { id: 4, text: "Masalah kecil seringkali membuat saya gampang emosi.", dimension: "Mental" },
+  { id: 5, text: "Saya lebih sering membayangkan hidup ideal daripada menghadapi kenyataan hidup yang ada.", dimension: "Mental" },
+  { id: 6, text: "Waktunya tidur, tapi pikiran masih jalan terus.", dimension: "Mental" },
+
+  // C. Lelah Emosional
+  { id: 7, text: "Saya lebih sering mengingat kegagalan daripada keberhasilan.", dimension: "Emosional" },
+  { id: 8, text: "Memikirkan arah hidup membuat hati terasa berat.", dimension: "Emosional" },
+  { id: 9, text: "Saya cenderung melihat sisi berat kehidupan.", dimension: "Emosional" },
+
+  // D. Lelah Spiritual
+  { id: 10, text: "Saya merasa harus tampil baik-baik saja di depan orang lain.", dimension: "Spiritual" },
+  { id: 11, text: "Saya lebih memilih menghindari acara kebersamaan.", dimension: "Spiritual" },
+  { id: 12, text: "Jika sedang memerlukan bantuan, saya bingung harus menghubungi siapa.", dimension: "Spiritual" },
+
+  // E. Lelah Sensorik
+  { id: 13, text: "Suara berisik membuat saya tidak nyaman.", dimension: "Sensorik" },
+  { id: 14, text: "Saya menatap layar monitor secara rutin setiap hari.", dimension: "Sensorik" },
+  { id: 15, text: "Saya kurang peka dengan bau di sekitar.", dimension: "Sensorik" },
+  { id: 16, text: "Saya lebih memilih makanan instan atau minuman manis.", dimension: "Sensorik" },
+  { id: 17, text: "Sentuhan fisik membuat saya tidak nyaman.", dimension: "Sensorik" },
+
+  // F. Lelah Sosial
+  { id: 18, text: "Saya lebih mendahulukan orang lain daripada kepentingan saya sendiri.", dimension: "Relasi" },
+  { id: 19, text: "Ketika menyediakan waktu untuk diri sendiri, saya sering merasa bersalah.", dimension: "Relasi" },
+  { id: 20, text: "Saya merasa usaha yang saya lakukan sering kurang dihargai orang lain.", dimension: "Relasi" },
+
+  // G. Lelah Ekspresif
+  { id: 21, text: "Mendapatkan ide baru terasa sulit bagi saya.", dimension: "Ekspresif" },
+  { id: 22, text: "Saya tidak merasa kreatif.", dimension: "Ekspresif" },
+  { id: 23, text: "Saya sedang membutuhkan semangat dan inspirasi.", dimension: "Ekspresif" }
+];
